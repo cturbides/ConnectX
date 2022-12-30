41f4aeea8405375f4b54bd79de102dddfa3d41ed
cturbides <c.sturbides@gmail.com>
2022-12-29 22:18:04 -0400

    feat: Landing page UI divided into Components

--- src/components/Home/Logo.tsx
+++ src/components/Home/Logo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from '../assets/connectx-logo-non-background.png';
+import logo from '../../assets/connectx-logo-non-background.png';
 
 const Logo = (): JSX.Element => {
 	return (
-		<div>
+		<div className='w-7/12 md:w-5/12 md:ml-[10%]'>
 			<img src={logo} alt="ConnectX Logo" />
 		</div>
 	);
