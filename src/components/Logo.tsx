755734af36a7ad7090989829833f11a02a6006b9
cturbides <c.sturbides@gmail.com>
2022-12-29 21:39:38 -0400

    feat: Logo component added

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/connectx-logo-non-background.png';
+
+const Logo = (): JSX.Element => {
+	return (
+		<div>
+			<img src={logo} alt="ConnectX Logo" />
+		</div>
+	);
+};
+
+export default Logo;
+
