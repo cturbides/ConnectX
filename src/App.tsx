d97af6a665f172f0867a4b6ee2171845c8de6375
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-03 20:28:13 +0000

    fix: Testing component in Router deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
-import WaitingRoom from './pages/WaitingRoom';
 
 const App = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
-	}, 
-	{
-		path: '/waiting-room',
-		element: <WaitingRoom />
 	}
 ]);
 
