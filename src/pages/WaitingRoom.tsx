5827beb0b38895d34deb2b16904cb3e9be944ef0
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-03 20:56:52 +0000

    feat: Waiting Room HTML elements added

--- src/pages/WaitingRoom.tsx
+++ src/pages/WaitingRoom.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Logo from '../components/General/Logo';
+import Footer from '../components/General/Footer';
 
 const WaitingRoom = (): JSX.Element => {
 	return (
-		<h1>Waiting Room</h1>
+		<div>
+			<div>
+				<Logo />
+			</div>
+			
+			<div>
+				<h1>
+					<span>What&apos;s your</span> <span>name</span> <span>?</span>
+				</h1>
+				<form>
+					<input type="text" name="user-name" id="username" />
+					<button type="submit">Continue</button>
+				</form>
+			</div>
+
+			<Footer />
+		</div>
 	);
 };
 
