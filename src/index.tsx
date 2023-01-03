10738ba57374fdb6b5563909b74cf429d0b1f76b
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-03 20:24:25 +0000

    feat: React Router updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+import { RouterProvider } from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+	<>
+		<RouterProvider router={App} />
+	</>
+);
