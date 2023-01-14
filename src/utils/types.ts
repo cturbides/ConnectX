62d6f0fec0d4a0f14ac66da54e37670390db8c0c
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-14 19:14:47 +0000

    feat: Username and RoomID Form added to Form component

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,13 @@
 export type WaitingRoomStep = 'username' | 'roomID';
+
 export type UsernameFormProps = {
     user: string;
     setStep: React.Dispatch<React.SetStateAction<WaitingRoomStep>>;
     setUser: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export type RoomIDFormProps = {
+    room: string;
+    user: string;
+    setRoom: React.Dispatch<React.SetStateAction<string>>;
 };
