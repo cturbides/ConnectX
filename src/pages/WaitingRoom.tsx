847157206efb4c5447da9f95485483c043c1d7c4
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-14 19:09:33 +0000

    update: WaitingRoom template updated

--- src/pages/WaitingRoom.tsx
+++ src/pages/WaitingRoom.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import Logo from '../components/General/Logo';
+import Form from '../components/WaitingRoom/Form';
 import Footer from '../components/General/Footer';
 
 const WaitingRoom = (): JSX.Element => {
 	return (
 		<div>
-			<div>
-				<Logo />
-			</div>
-			
-			<div>
-				<h1>
-					<span>What&apos;s your</span> <span>name</span> <span>?</span>
-				</h1>
-				<form>
-					<input type="text" name="user-name" id="username" />
-					<button type="submit">Continue</button>
-				</form>
-			</div>
-
+			<Logo />
+			<Form />
 			<Footer />
 		</div>
 	);
