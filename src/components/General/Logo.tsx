eeb1a4759d1d5fa432a9bcd6beb440fafbc1fa7d
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-24 13:54:59 +0000

    fix: Logo types and configuration added

--- src/components/General/Logo.tsx
+++ src/components/General/Logo.tsx
@@ -1,12 +1,31 @@
+import { LogoProps } from '../../utils/types';
+import originalLogo from '../../assets/connectx-logo-non-background.png';
 import React from 'react';
-import logo from '../../assets/connectx-logo-non-background.png';
 
-const Logo = (): JSX.Element => {
-	return (
+const Logo = (props: LogoProps): JSX.Element => {
+	const isBig 	= props.isBig;
+	const original 	= props.isOriginal;
+
+	const bigOriginalLogo: JSX.Element = (
 		<div className='w-7/12 md:w-5/12 md:ml-[10%]'>
-			<img src={logo} alt="ConnectX Logo" />
+			<img src={originalLogo} alt="ConnectX Logo" />
 		</div>
 	);
+
+	const smallOriginalLogo: JSX.Element = (
+		<div className='w-7/12 md:w-5/12 md:ml-[10%]'>
+			<img src={originalLogo} alt="ConnectX Logo" />
+		</div>
+	);
+
+	const smallWhiteLogo: JSX.Element = (
+		<div className='w-7/12 md:w-5/12 md:ml-[10%]'>
+			<img src={originalLogo} alt="ConnectX Logo" />
+		</div>
+	);
+
+	return  (isBig) 	? bigOriginalLogo   :
+		(original)  ? smallOriginalLogo : smallWhiteLogo;
 };
 
 export default Logo;
