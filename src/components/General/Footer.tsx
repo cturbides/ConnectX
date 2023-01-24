f53a0491aa8e2af6f3e52d72a98b799482b78dd3
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-24 16:21:19 +0000

    fix: Fixed style added to Footer

--- src/components/General/Footer.tsx
+++ src/components/General/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = (): JSX.Element => {
 	return (
-		<footer className='font-ramabhadra text-[9px] text-center fixed bottom-1 lg:text-center lg:text-[10px] w-screen'>
+		<footer className='fixed font-ramabhadra text-[9px] text-center fixed bottom-1 lg:text-center lg:text-[10px] w-screen'>
 			<h2>Created by Carlos Turbides</h2>
 		</footer>
 	);
