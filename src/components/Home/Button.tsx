6e20320b3bcf3b5426d65b9192be5a5534bd8f82
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-24 16:22:17 +0000

    fix: Faster background animation in Landing page added

--- src/components/Home/Button.tsx
+++ src/components/Home/Button.tsx
@@ -5,8 +5,8 @@ const Button = (): JSX.Element => {
 	return (
 		<div className='text-center ml-auto mr-auto mb-11 font-ramabhadra text-lg'>
 			<Link to='/waiting-room'>
-				<button className='cursor-pointer bg-main-black hover:bg-main-violet text-main-white 
-					mt-5 p-2 w-64 rounded-xl lg:w-96 ease-in-out duration-500'>
+				<button className='cursor-pointer bg-main-black hover:bg-main-violet active:bg-main-violet text-main-white 
+					mt-5 p-2 w-64 rounded-xl lg:w-96 ease-in-out duration-300'>
 					Begin now
 				</button>
 			</Link>
