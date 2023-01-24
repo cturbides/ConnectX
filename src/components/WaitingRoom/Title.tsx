082ef7f3130f09dfaf2d66916c464fe450d3beb0
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-24 16:37:47 +0000

    feat: Title component in WaitingRoom finished

--- src/components/WaitingRoom/Title.tsx
+++ src/components/WaitingRoom/Title.tsx
@@ -2,9 +2,25 @@ import { WaitingRoomStep } from '../../utils/types';
 import React from 'react';
 
 const Title = (props: {step: WaitingRoomStep}): JSX.Element => {
+	const normalStyle 	= 'text-main-black  font-ramabhadra';
+	const curlyStyle 	= 'text-main-violet font-ranga md:text-7xl lg:text-8xl';
+	const titleStyle 	= 'mb-10 text-center text-6xl lg:text-8xl';
+
+	const usernameElement: JSX.Element = (
+		<h1 className={titleStyle}>
+				<span className={normalStyle}>What&apos;s your</span> <span className={curlyStyle}>name</span><span className={normalStyle + ' ml-1'}>?</span>
+		</h1>
+	);
+
+	const roomElement: JSX.Element = (
+		<h1 className={titleStyle}>
+				<span className={normalStyle}>What&apos;s your</span> <span className={curlyStyle}>RoomID</span><span className={normalStyle + ' ml-1'}>?</span>
+		</h1>
+	);
+
 	return (props.step === 'username')
-		? <h1><span>What&apos;s your</span> <span>name</span> <span>?</span></h1>
-		: <h1><span>What&apos;s your</span> <span>RoomID</span> <span>?</span></h1>;
+		? usernameElement
+		: roomElement;
 };
 
 export default Title;
