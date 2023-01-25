1b4b297ee9cf701a86fac88ec562520656f1a353
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-25 13:14:01 +0000

    fix: Input overflow bug fixed for all platforms

--- src/components/WaitingRoom/RoomIDForm.tsx
+++ src/components/WaitingRoom/RoomIDForm.tsx
@@ -19,6 +19,11 @@ const RoomIDForm = (props: RoomIDFormProps): JSX.Element => {
 			setNewRoom(event.currentTarget.value);
 	};
 
+	const checkOverflow = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.value.length > 7)
+			event.target.value = event.target.value.slice(0, 7);
+	};
+
 	const submit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		// do whatever
@@ -32,8 +37,8 @@ const RoomIDForm = (props: RoomIDFormProps): JSX.Element => {
 			<form onSubmit={submit}>
 				<input 	className='text-center text-main-violet font-thin caret-black font-ranga focus:outline-none mb-2 text-7xl lg:text-8xl overflow-hidden'
 					type="text" name="roomID"  id="roomID"
-					placeholder={room} value={newRoom}
-					onChange={handleRoomID} maxLength={7}
+					placeholder={room} value={newRoom} onChange={handleRoomID} onInput={checkOverflow}
+					autoCorrect="off"  autoCapitalize="off" spellCheck="false" autoComplete='off'
 				/>
 				<br />
 				<div className='text-center ml-auto mr-auto mb-11 font-ramabhadra text-lg'>
