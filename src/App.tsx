3cd14c7a2d1e52f444fbbd76d3122aa66dd6154a
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-25 14:12:32 +0000

    feat: Basic Room component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { createHashRouter } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
+import Room from './pages/Room';
 import WaitingRoom from './pages/WaitingRoom';
 
 const App = createHashRouter([
 	{
 		path: '/',
 		element: <Home />,
+		index: true
 	},
 	{
-		path: '/waiting-room',
+		path: 'waiting-room',
 		element: <WaitingRoom />
+	},
+	{
+		path: 'room',
+		element: <Room />
 	}
 ]);
 
