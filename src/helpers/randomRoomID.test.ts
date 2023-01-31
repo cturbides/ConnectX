3876460d1f6e47c5d57245b2c2b0369e27132516
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-31 14:18:16 +0000

    fix: Non-null typo in randomRoomID test removed

--- src/helpers/randomRoomID.test.ts
+++ src/helpers/randomRoomID.test.ts
@@ -1,30 +1,30 @@
-import { generateRandomRoomID } from "./randomRoomID";
+import { generateRandomRoomID } from './randomRoomID';
 
 describe('RoomID string', () => {
-    test('has 7 characters', () => {
-        const roomID = generateRandomRoomID();
-        expect(roomID.length).toBe(7);
-    });
+	test('has 7 characters', () => {
+		const roomID = generateRandomRoomID();
+		expect(roomID.length).toBe(7);
+	});
 
-    test('contains a hyphen', () => {
-        const roomID = generateRandomRoomID();
-        expect(roomID).toContain('-');
-    });
+	test('contains a hyphen', () => {
+		const roomID = generateRandomRoomID();
+		expect(roomID).toContain('-');
+	});
 
-    test('first 3 letters are alphanumeric values', () => {
-        const roomID = generateRandomRoomID();
-        const firstThreeLetters = roomID.substring(0,3);
+	test('first 3 letters are alphanumeric values', () => {
+		const roomID = generateRandomRoomID();
+		const firstThreeLetters = roomID.substring(0,3);
         
-        const matchAlphaValues = firstThreeLetters.match(/\w/g);
-        expect(matchAlphaValues?.length).toBe(3);
+		const matchAlphaValues = firstThreeLetters.match(/\w/g);
+		expect(matchAlphaValues?.length).toBe(3);
         
-    });
+	});
 
-    test('last 3 letters are alphanumeric values', () => {
-        const roomID = generateRandomRoomID();
-        const lastThreeLetters = roomID.substring(4, 7);
+	test('last 3 letters are alphanumeric values', () => {
+		const roomID = generateRandomRoomID();
+		const lastThreeLetters = roomID.substring(4, 7);
 
-        const matchAlphaValues = lastThreeLetters.match(/\w/g);
-        expect(matchAlphaValues![0].length).toBe(3);
-    });
+		const matchAlphaValues = lastThreeLetters.match(/\w/g);
+		expect(matchAlphaValues?.length).toBe(3);
+	});
 });
