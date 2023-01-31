4c9a72b302af892b7fcd56457000c44f973e9a5f
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-31 17:13:28 +0000

    fix: Violet color added to Footer span

--- src/components/General/Footer.tsx
+++ src/components/General/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (): JSX.Element => {
 	return (
 		<footer className='w-screen fixed bottom-2 font-ramabhadra text-[9px] text-center lg:text-[8px]'>
-			<h2>Created by Carlos Turbides</h2>
+			<h2>Created by <span className='font-ranga text-main-violet'>Carlos Turbides</span></h2>
 		</footer>
 	);
 };
