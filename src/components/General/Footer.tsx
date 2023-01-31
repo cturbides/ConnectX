8d504b2e785b9e638fc427f3e6ca65b973e68772
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-01-31 15:06:57 +0000

    fix: Size and height changes applied to Footer component

--- src/components/General/Footer.tsx
+++ src/components/General/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = (): JSX.Element => {
 	return (
-		<footer className='w-screen fixed bottom-0 font-ramabhadra text-[9px] text-center lg:text-[10px]'>
+		<footer className='w-screen fixed bottom-2 font-ramabhadra text-[9px] text-center lg:text-[8px]'>
 			<h2>Created by Carlos Turbides</h2>
 		</footer>
 	);
