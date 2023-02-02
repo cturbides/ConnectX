0ff13e4a9a126d2dd687e8774988bb92ad6f21ba
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-02 20:16:28 +0000

    fix: Redundant return in UsersFrame component deleted

--- src/components/Room/UsersFrame.tsx
+++ src/components/Room/UsersFrame.tsx
@@ -5,9 +5,7 @@ import React from 'react';
 const UsersFrame = ({ users }: UsersFrameProps): JSX.Element => {
 	return (
 		<div className='text-main-white w-screen flex flex-wrap justify-center items-center'>
-			{users.map((user, index) => {
-				return <Frame user={user} key={index} />;
-			})}
+			{users.map((user, index) => <Frame user={user} micIsActive={false /**user.getMicrophoneState() */} videoIsActive={false /**user.getCameraState() */} key={index} />)}
 		</div>
 	);
 };
