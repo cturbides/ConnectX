7b87dd359504ffcaec4a703af65e1812ff3be361
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-06 14:02:12 +0000

    fix: Redundant Logo props file deleted

--- src/components/General/BigLogo.tsx
+++ src/components/General/BigLogo.tsx
@@ -1,12 +1,11 @@
-import { LogoProps } from './Logo.props';
 import main from '../../assets/connectx-logo-non-background.png';
 import white from '../../assets/connectx-white-logo-non-background.png';
 import React from 'react';
 
-const BigLogo = ({ original }: LogoProps): JSX.Element => {
+const BigLogo = (props: { original: boolean }): JSX.Element => {
 	return (
 		<div className='w-7/12 md:w-5/12 md:ml-[10%]'>
-			<img src={(original) ? main : white} alt="ConnectX Logo" />
+			<img src={(props.original) ? main : white} alt="ConnectX Logo" />
 		</div>
 	);
 };
