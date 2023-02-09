a3bdc9938401aadc91edcd9cdc8ad403b33f9b99
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-09 03:35:46 +0000

    feat: some states added from Room component

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,21 +1,26 @@
 import { useLocation } from 'react-router';
 import { UsersFrame } from './UsersFrame';
+import React, {useState} from 'react';
 import { CallInfo } from './CallInfo';
 import SmallLogo from '../General/SmallLogo';
 import Toolbar from './Toolbar';
 import Chat from './Chat';
-import React from 'react';
+
 
 const Room = (): JSX.Element => {
-	const location = useLocation();
-	const {user, roomID} = location.state;
+	const { user, roomID } = useLocation().state;
+	// eslint-disable-next-line
+	const [users, _setUsers] 		= useState<string[]>([user]); // Change user-type
+	// eslint-disable-next-line
+	const [messages, _setMessages] 	= useState<string[]>([]);
+	const [message, setMessage] 	= useState<string>('');
 
 	return (
 		<div className='min-h-screen h-screen w-screen flex bg-black'>
 			<SmallLogo original={false} applyToggle={true} />	
-			<CallInfo roomID={roomID} users={1}  />
-			<UsersFrame users={[user, 'Daniel', 'can34r', 'Laura']} />
-			<Chat />
+			<CallInfo roomID={roomID} users={users.length}  />
+			<UsersFrame users={users} />
+			<Chat messages={messages} message={message} setMessage={setMessage} />
 			<Toolbar />
 		</div>
 	);
