193b47abd2a45e653d85321ac817a7f5f487175c
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-14 15:28:29 +0000

    feat: General states added to upper-level component

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router';
 import { UsersFrame } from './UsersFrame';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { CallInfo } from './CallInfo';
 import SmallLogo from '../General/SmallLogo';
 import Toolbar from './Toolbar';
@@ -10,10 +10,22 @@ import Chat from './Chat';
 const Room = (): JSX.Element => {
 	const { user, roomID } = useLocation().state;
 	// eslint-disable-next-line
-	const [users, _setUsers] 		= useState<string[]>([user]); // Change user-type
+	const [users, setUsers] 			= useState<string[]>([user]); // Change user-type
 	// eslint-disable-next-line
-	const [messages, _setMessages] 	= useState<string[]>([]);
-	const [message, setMessage] 	= useState<string>('');
+	const [messages, _setMessages] 		= useState<string[]>([]);
+	const [message, setMessage] 		= useState<string>('');
+	const [videoState, setVideoState] 	= useState<boolean>(false);
+	const [micState, setMicState] 		= useState<boolean>(false);
+	const [chatState, setChatState] 	= useState<boolean>(false);
+
+	const toolbarStates = {
+		videoState,
+		micState,
+		chatState,
+		setVideoState,
+		setMicState,
+		setChatState
+	};
 
 	return (
 		<div className='min-h-screen h-screen w-screen flex bg-black'>
@@ -21,7 +33,7 @@ const Room = (): JSX.Element => {
 			<CallInfo roomID={roomID} users={users.length}  />
 			<UsersFrame users={users} />
 			<Chat messages={messages} message={message} setMessage={setMessage} />
-			<Toolbar />
+			<Toolbar {...toolbarStates} />
 		</div>
 	);
 };
