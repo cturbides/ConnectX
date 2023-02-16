f9ca9fb80a56488e3fdebdce1f022b6537fc6da9
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-16 21:55:10 +0000

    feat: Toolbar reorganized

--- src/components/Room/Toolbar.tsx
+++ src/components/Room/Toolbar.tsx
@@ -1,63 +1,59 @@
 import { faMicrophoneSlash, faMicrophone, faVideoSlash, faVideo, faPhoneSlash, faComment } from '@fortawesome/free-solid-svg-icons';
-import { Button, ButtonProps } from './Button';
+import { Button } from './Button';
 import React from 'react';
 
 interface Props {
-	micState: 	boolean;
-	videoState:	boolean;
-	chatState:	boolean;
-
-	setMicState: 	React.Dispatch<React.SetStateAction<boolean>>;
-	setVideoState: 	React.Dispatch<React.SetStateAction<boolean>>;
-	setChatState: 	React.Dispatch<React.SetStateAction<boolean>>;
+	micState: boolean;
+	videoState: boolean;
+	chatState: boolean;
+	setChatState: React.Dispatch<React.SetStateAction<boolean>>;
+	toggleMic: () => void;
+	toggleVideo: () => void;
 }
 
-const Toolbar = ({ micState, videoState, chatState, setMicState, setVideoState, setChatState }: Props): JSX.Element => {
-
-	const mic = {
-		active: faMicrophone,
-		unable: faMicrophoneSlash,
-		isActive: micState,
-		activeColor: 'text-main-violet',
-		unableColor: 'text-main-white',
-		procedure: () => setMicState(!micState),
-	} as ButtonProps;
-
-	const video = {
-		active: faVideo,
-		unable: faVideoSlash,
-		isActive: videoState,
-		activeColor: 'text-main-violet',
-		unableColor: 'text-main-white',
-		procedure: () => setVideoState(!videoState),
-	} as ButtonProps;
-	
-	const comment = {
-		active: faComment,
-		unable: faComment,
-		isActive: chatState,
-		activeColor: 'text-main-violet',
-		unableColor: 'text-main-white',
-		procedure: () => setChatState(!chatState)
-	} as ButtonProps;
-
-	const closeCall = {
-		active: faPhoneSlash,
-		unable: faPhoneSlash,
-		isActive: true,
-		activeColor: 'text-main-red',
-		unableColor: 'text-main-white',
-		procedure: () => console.log('')
-	} as ButtonProps;
-
+const Toolbar = ({ micState, videoState, chatState, setChatState, toggleMic, toggleVideo }: Props): JSX.Element => {
 	return (
 		<div className='fixed flex w-screen justify-center items-end bottom-0 mb-9 z-50'>
-			<Button {...mic} />
-			<Button {...video} />
-			<Button {...closeCall} />				
-			
+			<Button
+				key={'Mic'}
+				active={faMicrophone}
+				unable={faMicrophoneSlash}
+				isActive={micState}
+				activeColor={'text-main-violet'}
+				unableColor={'text-main-white'}
+				procedure={() => toggleMic()}
+			/>
+
+			<Button
+				key={'Video'}
+				active={faVideo}
+				unable={faVideoSlash}
+				isActive={videoState}
+				activeColor={'text-main-violet'}
+				unableColor={'text-main-white'}
+				procedure={() => toggleVideo()}
+			/>
+
+			<Button
+				key={'Close-call'}
+				active={faPhoneSlash}
+				unable={faPhoneSlash}
+				isActive={true}
+				activeColor={'text-main-red'}
+				unableColor={'text-main-white'}
+				procedure={() => console.log('')}
+			/>
+
 			<div className={`absolute right-0 mr-10 invisible xl:visible ${(chatState) ? 'hidden' : ''}`}>
-				<Button {...comment} />
+				<Button
+					key={'Comment'}
+					active={faComment}
+					unable={faComment}
+					isActive={chatState}
+					activeColor={'text-main-violet'}
+					unableColor={'text-main-white'}
+					procedure={() => setChatState(!chatState)}
+				/>
 			</div>
 		</div>
 	);
