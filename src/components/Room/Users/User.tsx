e636db37d1d9819d86c89398dfad6d4a659ee847
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-17 16:44:54 +0000

    feat: Users components adapted to User Type

--- src/components/Room/Users/User.tsx
+++ src/components/Room/Users/User.tsx
@@ -1,29 +1,26 @@
 import React, { MutableRefObject, useRef, useEffect } from 'react';
 
 export type UserProps = {
+	id: string;
 	name: string;
 	media: MediaStream;
-	mic: boolean | false;
-	video: boolean | false;
+	mic: boolean;
+	video: boolean;
 	isLocal: boolean | false;
-}
+};
+
 
 export const User = ({ name, media, mic, video, isLocal }: UserProps): JSX.Element => {
 	const videoSrc = useRef() as MutableRefObject<HTMLVideoElement>;
 	const micActiveStyle = (mic && !video) ? 'border-main-violet' : '';
 	const activeVideoStyle = (video && mic) ? 'border-main-violet' : 'border-main-white';
 
-	useEffect(() => { videoSrc.current.srcObject = media; }, [])
+	useEffect(() => { videoSrc.current.srcObject = media; }, [media, video]);
 
 	return (
-		<div className={`min-h-[170px] max-h-[290px] md:h-1/3 2xl:h-2/5 min-w-[250px] max-w-[400px] w-1/3 2xl:w-3/4 border-[1px]
-						${activeVideoStyle} hover:border-main-violet active:border-main-violet ease-in-out duration-300 border-solid rounded-lg`}>
-
+		<div className={`min-h-[170px] max-h-[290px] md:h-1/3 2xl:h-2/5 min-w-[250px] max-w-[400px] w-1/3 2xl:w-3/4 border-[1px] ${activeVideoStyle} hover:border-main-violet active:border-main-violet ease-in-out duration-300 border-solid rounded-lg`}>
 			<div className='relative block h-full'>
-				<div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[100px] w-[100px] ${micActiveStyle}
-								${(video) ? 'hidden' : ' '} rounded-full border-main-white border-[1px] my-auto mx-auto text-center
-								text-main-white font-ramabhadra text-6xl`}>
-
+				<div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[100px] w-[100px] ${micActiveStyle} ${(video) ? 'hidden' : ' '} rounded-full border-main-white border-[1px] my-auto mx-auto text-center text-main-white font-ramabhadra text-6xl`}>
 					<h4 className='mt-[16px]'>{name.charAt(0).toUpperCase()}</h4>
 				</div>
 
