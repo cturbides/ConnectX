b2d0e93cc3fb3aed96f548ebac79e44e70b630ad
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-20 17:04:03 +0000

    feat: SmallLogo component refactored & exit function support added

--- src/components/General/SmallLogo.tsx
+++ src/components/General/SmallLogo.tsx
@@ -1,26 +1,34 @@
-import { Link } from 'react-router-dom';
 import white from '../../assets/connectx-white-logo-non-background.png';
 import main from '../../assets/connectx-logo-non-background.png';
+import { useNavigate } from 'react-router';
 import React, { useState } from 'react';
 
 interface Props {
 	original: boolean;
 	applyToggle: boolean;
+	exit: () => void;
 }
 
-const SmallLogo = ({ original, applyToggle }: Props): JSX.Element => {
+const SmallLogo = ({ original, applyToggle, exit }: Props): JSX.Element => {
 	const [showOriginal, setShowOriginal] = useState(original);
 	const toggle = () => (applyToggle) ? setShowOriginal(prev => !prev) : 0;
+	const navigate = useNavigate();
 
 	return (
-		<Link to='/'>
-			<div className='fixed w-[70px] ml-5 mt-5 md:w-20 md:ml-10 md:mt-5 transition duration-300 easy-in-out' onMouseOver={toggle} onMouseOut={toggle}>
-				<div className='relative'>
-					<img className={`absolute top-0 transition easy-in-out duration-300 ${(showOriginal) ? 'opacity-100' : 'opacity-0'}`} src={main} alt="ConnectX Logo" />
-					<img className={`absolute top-0 transition easy-in-out duration-300 ${(showOriginal) ? 'opacity-0' : 'opacity-100'}`} src={white} alt="ConnectX Logo" />
-				</div>
+		<div
+			className='fixed w-[70px] ml-5 mt-5 md:w-20 md:ml-10 md:mt-5 transition duration-300 easy-in-out focus:cursor-pointer hover:cursor-pointer'
+			onMouseOver={toggle}
+			onMouseOut={toggle}
+			onClick={() => {
+				exit();
+				navigate('/');
+			}}
+		>
+			<div className='relative'>
+				<img className={`absolute top-0 transition easy-in-out duration-300 ${(showOriginal) ? 'opacity-100' : 'opacity-0'}`} src={main} alt="ConnectX Logo" />
+				<img className={`absolute top-0 transition easy-in-out duration-300 ${(showOriginal) ? 'opacity-0' : 'opacity-100'}`} src={white} alt="ConnectX Logo" />
 			</div>
-		</Link>
+		</div>
 	);
 };
 
