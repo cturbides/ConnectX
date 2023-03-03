8128519065ee4971706712407003cf0613422961
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-03-03 03:40:19 +0000

    fix: Error in messages layer fixed

--- src/components/Room/Chat/Chat.tsx
+++ src/components/Room/Chat/Chat.tsx
@@ -18,24 +18,28 @@ interface ChatProps {
 const Chat = ({ username, messages, message, chatState, setMessage, setChatState, sendMessage }: ChatProps): JSX.Element => {
 	return (
 		<div className={`h-screen transition-all duration-300 ${(!chatState) ? 'w-0' : 'w-1/2'}`}>
-			<div className={`relative h-5/6 min-h-[370px] max-w-[350px] w-5/6 xl:w-full border-main-white border-[1px] rounded-[15px] mr-[43px] mt-[71px] ml-auto transition-all duration-300 ${(chatState) ? 'translate-x-0' : 'translate-x-[100vw]'}`}>
+			<div className='w-full h-full pr-[43px] flex items-center justify-end'>
+				<div className={`relative h-5/6 min-h-[200px] max-w-[350px] w-5/6 xl:w-full border-main-white border-[1px] rounded-[15px] transition-all duration-300 ${(chatState) ? 'translate-x-0' : 'translate-x-[100vw]'}`}>
 
-				<div className='relative w-full'>
-					<h1 className='text-main-white text-2xl font-ramabhadra ml-[28px] mt-[17px]'>Messages</h1>
-					<div className='absolute top-0 right-0 mt-[6px] mr-[25px]'>
-						<button onClick={() => setChatState(!chatState)}>
-							<FontAwesomeIcon icon={faXmark} className='text-main-white w-[25px] h-[25px] ease-in-out duration-300 hover:text-main-violet focus:text-main-violet' />
-						</button>
+					<div className='flex flex-col h-full'>
+
+						<div className='w-full pt-[15px] px-6 flex justify-between content-center'>
+							<h1 className='text-main-white text-2xl font-ramabhadra'>Messages</h1>
+							<button onClick={() => setChatState(!chatState)} className='mt-2'>
+								<FontAwesomeIcon icon={faXmark} className='text-main-white w-[25px] h-[25px] ease-in-out duration-300 hover:text-main-violet focus:text-main-violet' />
+							</button>
+						</div>
+
+						<div className='w-full px-10 pt-6 pb-[60px] flex-1 overflow-hidden' id="Messages">
+							<div className="h-full overflow-y-auto">
+								{messages.map((message, index) => <MessageTile username={(message.username === username) ? 'Me' : message.username} content={message.content} key={index} />)}
+							</div>
+						</div>
+
 					</div>
-				</div>
 
-				<div className='absolute flex items-center justify-center h-full'>
-					<div id='messages' className='h-3/4 max-h-[760px] w-full overflow-y-scroll'>
-						{messages.map((message, index) => <MessageTile username={(message.username === username) ? 'Me' : message.username} content={message.content} key={index} />)}
-					</div>
+					<MessageInput message={message} setMessage={setMessage} sendMessage={sendMessage} />
 				</div>
-
-				<MessageInput message={message} setMessage={setMessage} sendMessage={sendMessage} />
 			</div>
 		</div>
 	);
